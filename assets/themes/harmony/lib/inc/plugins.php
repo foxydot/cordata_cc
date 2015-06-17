94988e17c8f3cfd1c103ab2286b9b251e61d55a4
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-06-16 23:48:16 -0400

    run shortcodes in gravit forms description

---
+++ assets/themes/harmony/lib/inc/plugins.php
@@ -0,0 +1,14 @@
+<?php
+add_filter(‘gform_pre_render’, ‘walker_do_shortcode_gform_description’, 10, 2);
+
+function do_shortcode_gform_description(&$item, $key) {
+$item = do_shortcode($item);
+}
+
+function walker_do_shortcode_gform_description($form, $ajax) {
+
+$form[‘description’] = do_shortcode($form[‘description’]);
+array_walk_recursive($form[‘fields’], ‘do_shortcode_gform_description’);
+
+return $form;
+}
